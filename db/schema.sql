e1330b58eb8ad26142faf50f7e8a0520d8a823b2
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-25 16:28:44 -0700

    changed lowercase letters to uppercase letters in schema.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,27 @@ CREATE DATABASE employees_DB;
 USE employees_DB;
 
 CREATE TABLE department(
-	id int auto_increment,
-    name varchar(30) not null,
-    primary key(id)
+	id INT auto_increment,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role(
-	id int auto_increment,
-    title varchar(30) not null,
-    salary decimal(10, 2),
-    department_id int,
-    primary key(id),
-    constraint fk_department foreign key (department_id) references department(id) on delete set null
+	id INT auto_increment,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2),
+    department_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-	id int auto_increment,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    role_id int,
-    constraint fk_role foreign key (role_id) references role(id) on delete set null,
-    manager_id int,
-    constraint fk_employee foreign key (manager_id) references employee(id) on delete set null,
-    primary key(id),
+	id INT auto_increment,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) oON DELETE CASCADE,
+    manager_id INT,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
+    PRIMARY KEY(id),
 );

7c845f8136669cb5e06c3641df3ab879be979b74
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-25 16:35:56 -0700

    changed the packages start from npm index.js to npm server.js so npm start would work correctly.

--- server.js
+++ server.js
@@ -113,7 +113,8 @@ const startApp = () => {
 
 //Consoles joined tables with each employees information
 const viewAllEmp = () => {
-    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON (manager.id = employee.manager_id) INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id", (err, res) => {//joins
+    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON (manager.id = employee.manager_id) INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id", 
+    (err, res) => {
         if (err) throw err;
         console.table(res)
         startApp();
@@ -153,7 +154,7 @@ const addEmp = () => {
                 type: "input",
                 message: "What is the employee's last name?",
                 name: "lastName"
-            },
+            }
         ])
         .then((answer) => {
             db.query("INSERT INTO employee (first_name, last_name) VALUES (?, ?)",
@@ -176,7 +177,6 @@ const whatRole = (firstName) => {
         res.forEach(({id, title}) => {
             const roles = `${id} ${title}`;
             roleArray.push(roles);
-            
         }); 
         inquirer
             .prompt({
